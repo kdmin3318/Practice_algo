142945e1b333620a4efcd240cddc22fc3c8009b5
kdmin3318 <kdmin3318@gmail.com>
2025-05-29 22:01:59 +0900

    beakjoon2490

---
+++ Basic/Yutnori.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    a = list(map(int, input().split()))
+    if sum(a)==4:
+        print('E')
+    elif sum(a)==3:
+        print('A')
+    elif sum(a)==2:
+        print('B')
+    elif sum(a)==1:
+        print('C')
+    else:
+        print('D')
