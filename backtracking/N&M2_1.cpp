998f3a80f9412c8806db33b7d6038f39bb83679c
kdmin3318 <kdmin3318@gmail.com>
2025-07-19 16:30:44 +0900

    beakjoon15650

---
+++ backtracking/N&M2_1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+
+    vector<int> a;
+    for(int i=0;i<n;i++) a.push_back(i<m?0:1);
+    do{
+        for(int i=0;i<n;i++)
+            if(a[i]==0) cout<<i+1<<' ';
+        cout<<"\n";
+    }while(next_permutation(a.begin(), a.end()));
+}
