155522ab17fefc7b5ce719c4e6688d63b10d4668
kdmin3318 <kdmin3318@gmail.com>
2025-07-19 19:09:50 +0900

    beakjoon16562

---
+++ backtracking/N&M4.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def solve(k,n,m,arr):
+    if k==m:
+        for i in range(m): print(arr[i], end=" ")
+        print()
+        return
+    
+    st = arr[k-1] if k!=0 else 1
+    for i in range(st,n+1):
+        arr[k] = i
+        solve(k+1,n,m,arr)
+
+
+def main():
+    n,m = map(int, input().split())
+    arr = [0] * m
+    solve(0,n,m,arr)
+
+if __name__ == "__main__":
+    main()
