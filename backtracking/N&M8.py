cc1b91ca9ca2b31d679a8b4ef40e6fc4c94c2804
kdmin3318 <kdmin3318@gmail.com>
2025-07-22 21:47:34 +0900

    beakjoon15657

---
+++ backtracking/N&M8.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+class NandM:
+    def __init__(self,n,m,num):
+        self.n = n
+        self.m = m
+        self.num = sorted(num)
+        self.arr = [0]*m
+
+    def solve(self, k):
+        if k==self.m:
+            for i in range(self.m):
+                print(self.num[self.arr[i]], end=" ")
+            print()
+            return
+        
+        st = self.arr[k-1] if k!=0 else 0
+        for i in range(st, self.n):
+            self.arr[k] = i
+            self.solve(k+1)
+
+def main():
+    n,m = map(int, input().split())
+    num = list(map(int,input().split()))
+
+    NM = NandM(n,m,num)
+    NM.solve(0)
+
+if __name__ == "__main__":
+    main()
