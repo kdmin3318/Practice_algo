f3d32197440f4b51340bae92af8c513fac2f37bb
kdmin3318 <kdmin3318@gmail.com>
2026-04-19 15:00:36 +0900

    그리디 풀이방법 추가

--- Programmers/Level1/budget.py
+++ Programmers/Level1/budget.py
@@ -7,7 +7,7 @@ def solution(d, budget):
   return answer
 """
 while문에서 or과 and차이 조심하기!
-for문으로 풀기
+1. for문으로 풀기
 def solution(d, budget):
   answer = 0
   d.sort()
@@ -16,4 +16,11 @@ def solution(d, budget):
     budget -= i
     answer += 1
   return ansewr
+2. 그리디 풀이방식(O(n^2으로 비효율적임..)
+def solution(d, budget):
+  d.sort()
+  while sum(d)>budget:
+    d.pop(-1)
+  answer = len(d)
+  return answer
 """
