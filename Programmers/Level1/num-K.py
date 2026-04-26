de7a70a11812ebe5ebf5bdafbf0fa948230b35c6
kdmin3318 <kdmin3318@gmail.com>
2026-04-26 21:12:46 +0900

    프로그래머스 K번째 수 문제 풀이
    unpacking을 활용한 문제 풀이

---
+++ Programmers/Level1/num-K.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+  answer = []
+  for (i,j,k) in commands:
+    temp = sorted(array[i-1:j])
+    answer.append(temp[k-1])
+  return answer
+"""
+unpacking을 활용하여 문제 풀이
+"""
