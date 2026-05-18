6fdd03e55ebba228377cef5c42a4cbe4dbd68abb
kdmin3318 <kdmin3318@gmail.com>
2026-05-18 15:43:35 +0900

    slicing방법 추가

--- Programmers/Level2/land.py
+++ Programmers/Level2/land.py
@@ -9,4 +9,12 @@ def solution(land):
 """
 DP풀이 방법..
 참고! DP는 점화식을 세워야하기 떄문에 i가 나오려면 i-1에 뭘 하면되는지를 생각하는 것이 좋음!
+1. slicing추가 활용
+def solution(land):
+  n = len(land)
+  for i in range(n):
+    for j in range(4):
+      prev_max = max(land[i-1][:j] + land[i-1][j+1:]
+      land[i][j] += prev_max
+  return max(land[n-1])
 """
